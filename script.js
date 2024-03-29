c11cb465b50b980f7968dedf99c7c1d9c40c9ffd
Melissa066 <Melissa066@users.noreply.github.com>
2024-03-29 19:08:55 +0000

    html and css done

---
+++ script.js
@@ -0,0 +1,21 @@
+//document.getElementById("count-el").innerText = 5
+// let firstBatch = 5
+// let secondBatch = 7
+
+
+// let count = firstBatch + secondBatch
+
+// initialize the count as 0
+// listen for click on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the html to reflect the new count
+
+function increment() {
+    
+    
+}
+
+
+
+
+
